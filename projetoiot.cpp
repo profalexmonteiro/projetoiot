753d61c88b52e2981100428ebde3a63c575115d2
Alex Monteiro <alex.monteiro@ifam.edu.br>
2021-07-17 03:24:35 -0300

    first commit

---
+++ projetoiot.cpp
@@ -0,0 +1,18 @@
+// Do not remove the include below
+#include "projetoiot.h"
+
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+	Serial.begin(9600);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+	Serial.println("Arduino Git");
+
+}
---
+++ projetoiot.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _projetoiot_H_
+#define _projetoiot_H_
+#include "Arduino.h"
+//add your includes for the project projetoiot here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project projetoiot here
+
+
+
+
+//Do not add code below this line
+#endif /* _projetoiot_H_ */
